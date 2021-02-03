84312b405356608694557b493dcda910d6465a86
Lorenze Jay <Lorenzejay@gmail.com>
2021-02-03 11:42:50 -0800

    adjusted columns for bigger screen

--- src/components/addOns.js
+++ src/components/addOns.js
@@ -7,10 +7,44 @@ export const AddOnItem = styled.section`
   display: flex;
   flex-direction: column;
   min-height: 35vh;
+
   @media screen and (min-width: 1024px) {
     .addoncontent {
-      width: 50%;
-      margin-left: ${props => (props.number % 2 === 0 ? 0 : "auto")};
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      margin-top: 5vh;
+      padding: 0 5%;
+      /* margin-left: ${props => (props.number % 2 === 0 ? "auto" : 0)}; */
+
+      /* align-items: ${props => (props.number % 2 === 0 ? "" : "flex-end")};
+      
+      */
+      h3,
+      p {
+        width: 100%;
+        text-align: ${props => (props.number % 2 === 0 ? "end" : "start")};
+      }
+      ul {
+        display: flex;
+        flex-direction: column;
+        width: 65%;
+        padding: 0;
+        margin: 0;
+        justify-content: flex-end;
+        /* align-self: flex-start; */
+
+        align-self: ${props =>
+          props.number % 2 === 0 ? "flex-end" : "flex-start"};
+        li {
+          text-align: ${props => (props.number % 2 === 0 ? "end" : "start")};
+        }
+      }
+    }
+  }
+  @media screen and (min-width: 1440px) {
+    .addoncontent {
+      padding: 0 20%;
     }
   }
 `
@@ -30,21 +64,18 @@ const AddOns = () => {
   `)
   console.log(data.allAddOnsJson)
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col justify-items-center items-center  gap-5">
       {data.allAddOnsJson.nodes.map(item => {
         console.log(item.id)
         return (
-          <AddOnItem key={item.id} number={item.id} className="p-10 ">
-            <div className="addoncontent ">
+          <AddOnItem key={item.id} number={item.id} className="p-10  w-full">
+            <div className="addoncontent flex flex-col">
               <h3 className="text-3xl">{item.title}</h3>
-              <p className=" my-5 tracking-widest">+{item.price}</p>
+              <p className=" my-5 lg:text-xl tracking-widest">+{item.price}</p>
 
               <ul>
                 {item.description.map((x, i) => (
-                  <li
-                    key={i}
-                    className="bullets lg:w-3/4 leading-loose text-lg"
-                  >
+                  <li key={i} className="bullets  leading-loose text-lg">
                     {x}
                   </li>
                 ))}
